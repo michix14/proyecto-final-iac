b3aa844e90043ef24683f1a713628aac141fbe92
michix14 <86693688+michix14@users.noreply.github.com>
2025-10-31 00:16:17 -0400

    Probando el CI/CD
    Prueba

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.json({ status: "ok", service: "Hola Microservicio 2" });
+  res.json({ status: "ok", service: "Hola Microservicio" });
 });
 
 export default app;
